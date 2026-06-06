aacb4991d8d9120d2a2abaaf61d8f851cb5b1166
a7chax <akuarung83@gmail.com>
2026-06-07 02:34:56 +0700

    add /bye

--- 38-node-server/simpleweb/index.js
+++ 38-node-server/simpleweb/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hi there');
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
+
+app.get("/bye", (req, res) => {
+  res.send("Good Bye");
 });
 
 app.listen(8080, () => {
-  console.log('Listening on port 8080');
+  console.log("Listening on port 8080");
 });
